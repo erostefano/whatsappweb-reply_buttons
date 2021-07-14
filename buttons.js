6b52e64054eeb23467f17638b1abfdc2c4339753
Stefano Mavilio <st.mavilio@gmail.com>
2021-07-14 22:54:25 +0200

    create button from settings

--- buttons.js
+++ buttons.js
@@ -9,11 +9,19 @@ const buttonGroup = document.createElement('div');
 buttonGroup.classList.add('button-group');
 
 // create buttons
-let button = document.createElement('button');
-button.classList.add('reply-button');
-button.innerHTML = 'Order accept';
-button.title = 'Accepted order Accepted order Accepted order Accepted order Accepted orderAccepted order';
+chrome.storage.local.get('settings', ({settings}) => {
+    let buttons = JSON.parse(settings);
+    buttons.forEach(el => {
+        let button = document.createElement('button');
+        button.classList.add('reply-button');
+        button.innerHTML = el;
+        button.title = 'Accepted order Accepted order Accepted order Accepted order Accepted orderAccepted order';
+        button.addEventListener('click', () => {
 
-buttonGroup.appendChild(button);
+        });
 
-document.body.appendChild(buttonGroup);
+        buttonGroup.appendChild(button);
+        document.body.appendChild(buttonGroup);
+    });
+});
+
