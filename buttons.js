7bea43cda2070d1dedc414f46ae10bc1d1380d86
Stefano Mavilio <st.mavilio@gmail.com>
2021-07-15 22:28:51 +0200

    toggle button-group

--- buttons.js
+++ buttons.js
@@ -7,21 +7,50 @@ document.head.appendChild(style);
 // create buttonGroup
 const buttonGroup = document.createElement('div');
 buttonGroup.classList.add('button-group');
+buttonGroup.style.position = 'absolute';
+buttonGroup.style.top = '6rem';
+buttonGroup.style.left = '500px';
+document.body.appendChild(buttonGroup);
 
 // create buttons
 chrome.storage.local.get('settings', ({settings}) => {
-    let buttons = JSON.parse(settings);
-    buttons.forEach(el => {
-        let button = document.createElement('button');
+    const buttonSettings = JSON.parse(settings);
+    buttonSettings.forEach(setting => {
+        const button = document.createElement('button');
         button.classList.add('reply-button');
-        button.innerHTML = el;
-        button.title = el;
+        button.innerHTML = setting;
+        button.title = setting;
         button.addEventListener('click', () => {
-
+            buttonGroup.style.display = toggleButtonGroup(buttonGroup.style.display);
+            // TODO: send text!
         });
 
         buttonGroup.appendChild(button);
-        document.body.appendChild(buttonGroup);
     });
 });
 
+window.addEventListener('click', () => {
+    if (document.getElementById('button-group-toggle')) {
+        return;
+    }
+
+    let clip = document.querySelector('[data-testid="clip"]').parentElement.parentElement;
+
+    if (!clip) {
+        return;
+    }
+
+    let buttonGroupToggle = clip.cloneNode(true);
+    buttonGroupToggle.setAttribute('id', 'button-group-toggle');
+    buttonGroupToggle.addEventListener('click', () => {
+        buttonGroup.style.display = toggleButtonGroup(buttonGroup.style.display);
+    });
+
+    clip.parentElement.appendChild(buttonGroupToggle);
+});
+
+function toggleButtonGroup(display) {
+    return display !== 'flex'
+        ? 'flex'
+        : 'none';
+}
