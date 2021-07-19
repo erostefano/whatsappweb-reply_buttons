068b373afe56076431d54586ab94d31f06f64333
Stefano Mavilio <st.mavilio@gmail.com>
2021-07-19 20:59:35 +0200

    save array on focus out

--- settings/settings.js
+++ settings/settings.js
@@ -1,9 +1,14 @@
 const replyDivs = document.querySelectorAll('.reply')
 replyDivs.forEach(div => {
+    let input = div.children[0];
+    input.addEventListener('blur', () => {
+        const inputs = document.querySelectorAll('.reply input');
+        chrome.storage.local.set({settings: inputs.map(input => input.value)});
+    });
     let button = div.children[1];
     button.addEventListener('click', () => {
         div.remove();
-    })
+    });
 })
 
 const addButton = document.getElementsByClassName('add')[0];
