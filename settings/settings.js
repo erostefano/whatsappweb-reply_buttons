a98732c80d084eb8cd8be7a0bb8e78f1753cd837
Stefano Mavilio <st.mavilio@gmail.com>
2021-07-19 20:47:30 +0200

    add and remove button in settings

--- settings/settings.js
+++ settings/settings.js
@@ -1,11 +1,18 @@
-const textArea = document.getElementsByTagName('textarea')[0];
-
-chrome.storage.local.get('settings', ({settings}) => {
-    textArea.value = settings;
-});
-
-const save = document.getElementsByTagName('button')[0];
-
-save.addEventListener('click', () => {
-    chrome.storage.local.set({settings: textArea.value});
+const replyDivs = document.querySelectorAll('.reply')
+replyDivs.forEach(div => {
+    let button = div.children[1];
+    button.addEventListener('click', () => {
+        div.remove();
+    })
 })
+
+const addButton = document.getElementsByClassName('add')[0];
+addButton.addEventListener('click', () => {
+    const replyDiv = document.getElementsByClassName('reply')[0].cloneNode(true);
+    replyDiv.children[0].value = '';
+    replyDiv.children[1].addEventListener('click', () => {
+        replyDiv.remove();
+    })
+    const buttonGroup = document.getElementById('button-group');
+    buttonGroup.appendChild(replyDiv);
+});
