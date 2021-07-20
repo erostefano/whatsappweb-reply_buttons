09f8fdb167bf75e0b78f2a8b3f5cea092ffe26c4
Stefano Mavilio <st.mavilio@gmail.com>
2021-07-20 12:07:42 +0200

    celan toggling

--- buttons/buttons.js
+++ buttons/buttons.js
@@ -4,30 +4,6 @@ style.setAttribute('rel', 'stylesheet');
 style.setAttribute('href', chrome.runtime.getURL('buttons/buttons.css'));
 document.head.appendChild(style);
 
-// create buttonGroup
-const buttonGroup = document.createElement('div');
-buttonGroup.setAttribute('id', 'button-group');
-document.body.appendChild(buttonGroup);
-
-// create buttons
-chrome.storage.local.get('settings', ({settings}) => {
-    settings.forEach(setting => {
-        const button = document.createElement('button');
-        button.classList.add('reply-button');
-        button.innerHTML = setting;
-        button.title = setting;
-        button.addEventListener('click', event => {
-            toggleReplyButtons(buttonGroup);
-            const inputText = document.querySelectorAll(".copyable-text.selectable-text[contenteditable='true']")[1];
-            inputText.innerHTML = event.target.innerHTML;
-            inputText.dispatchEvent(new Event('input', {bubbles: true}));
-            const send = document.querySelector("[data-testid='send']").parentElement;
-            send.click();
-        });
-        buttonGroup.appendChild(button);
-    });
-});
-
 window.addEventListener('click', () => {
     const clip = document.querySelector('[data-testid="clip"]').parentElement.parentElement;
 
@@ -39,16 +15,46 @@ window.addEventListener('click', () => {
         return;
     }
 
-    // position buttonGroup
-    buttonGroup.style.top = (clip.getBoundingClientRect().top - buttonGroup.getBoundingClientRect().height - 16) + 'px';
-    buttonGroup.style.left = (document.getElementById('side').getBoundingClientRect().right + 16) + 'px';
-
     // create buttonGroupToggle
     const buttonGroupToggle = document.createElement('img');
     buttonGroupToggle.setAttribute('id', 'button-group-toggle');
     buttonGroupToggle.src = chrome.runtime.getURL('icons/robot64.png');
     buttonGroupToggle.addEventListener('click', () => {
-        toggleReplyButtons(buttonGroup, buttonGroupToggle);
+        if (buttonGroupToggle.classList.contains('open')) {
+            document.getElementById('button-group').remove();
+            buttonGroupToggle.classList.remove('open');
+        } else {
+            buttonGroupToggle.classList.add('open');
+
+            // create buttonGroup
+            const buttonGroup = document.createElement('div');
+            buttonGroup.setAttribute('id', 'button-group');
+            document.body.appendChild(buttonGroup);
+
+            // create buttons
+            chrome.storage.local.get('settings', ({settings}) => {
+                settings.forEach(setting => {
+                    const button = document.createElement('button');
+                    button.classList.add('reply-button');
+                    button.innerHTML = setting;
+                    button.title = setting;
+                    button.addEventListener('click', event => {
+                        const inputText = document.querySelectorAll(".copyable-text.selectable-text[contenteditable='true']")[1];
+                        inputText.innerHTML = event.target.innerHTML;
+                        inputText.dispatchEvent(new Event('input', {bubbles: true}));
+                        const send = document.querySelector("[data-testid='send']").parentElement;
+                        send.click();
+                        buttonGroup.remove();
+                        buttonGroupToggle.classList.remove('open');
+                    });
+                    buttonGroup.appendChild(button);
+                });
+
+                // position button-group-toggle
+                buttonGroup.style.top = (clip.getBoundingClientRect().top - buttonGroup.getBoundingClientRect().height - 16) + 'px';
+                buttonGroup.style.left = (document.getElementById('side').getBoundingClientRect().right + 16) + 'px';
+            });
+        }
     });
     clip.parentElement.parentElement.appendChild(buttonGroupToggle);
 });
